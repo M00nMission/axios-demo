a3252283a727a27484f920fa3d636457400e2f07
XanMan <57318830+XanMan336@users.noreply.github.com>
2021-12-05 16:21:41 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import Todo from "./components/Todo";
+import Form from "./components/Form";
+import Title from "./components/Title";
+import "./App.css";
+
+const initialData = [
+  {
+    _id: 0,
+    text: "Call mom"
+  },
+  {
+    _id: 1,
+    text: "Work out"
+  },
+  {
+    _id: 2,
+    text: "Finish ReactJS talk"
+  }
+];
+
+const App = () => {
+  const [inputValue, setInputValue] = useState("");
+  const [todos, setTodos] = useState(initialData);
+
+  const handleSubmit = () => {
+    if (inputValue === "") return;
+    const newTodo = { _id: Date.now(), text: inputValue };
+    setTodos([...todos, newTodo]);
+    setInputValue("");
+  };
+
+  const handleDeleteTodo = id => {
+    const newTodos = todos.filter(todo => todo._id !== id);
+    setTodos(newTodos);
+  };
+
+  return (
+    <div className="app">
+      <div className="todolist">
+        <Title myName="Nico" />
+        <Form
+          inputValue={inputValue}
+          setInputValue={setInputValue}
+          handleSubmit={handleSubmit}
+        />
+        {todos.map((todo, index) => (
+          <Todo
+            todo={todo}
+            index={index}
+            handleDeleteTodo={handleDeleteTodo}
+            key={todo._id}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default App;
